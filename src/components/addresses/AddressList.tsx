f7090f4f95b2d018eef5c9956144d3da365cab66
Brian Sasbon <bsasbon@neopower.tech>
2023-05-04 13:41:36 -0300

    fix: zindex import

--- src/components/addresses/AddressList.tsx
+++ src/components/addresses/AddressList.tsx
@@ -1,8 +1,6 @@
 import State from '@/core/components/State';
 import { CryptoAddress, FIATAddress } from '@/lib/model/address';
-
 import { Stack } from '@mui/material';
-import zIndex from '@mui/material/styles/zIndex';
 import { useState } from 'react';
 import AddressCard from './AddressCard';
 import AddressMenu from './AddressMenu';
@@ -41,7 +39,7 @@ const AddressList: React.FC<AddressListProps> = ({ addresses }) => {
           addressData={address}
           onClick={(event) => handleClick(event, address)}
           style={{
-            zIndex: selectedAddress === address ? zIndex.modal + 1 : 0,
+            zIndex: selectedAddress === address ? 1301 : 0,
             position: selectedAddress === address ? 'relative' : undefined,
           }}
         />
