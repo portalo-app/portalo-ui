5603fadc4c897d8fa17a29869f5f16da6c35c655
braifz <braifz.dev@gmail.com>
2023-12-12 10:05:58 -0300

    deleted some unused components and modified State.tsx

--- src/core/components/State.tsx
+++ src/core/components/State.tsx
@@ -1,26 +1,26 @@
 import { AlertTriangle, CheckCircle, Info, XCircle } from 'lucide-react';
 
 type StateType = 'success' | 'info' | 'warning' | 'error';
-type SizeType = number | undefined;
+type SizeType = number;
 interface StateProps {
   type: StateType;
   label: string;
-  size: SizeType;
+  size?: SizeType;
 }
 
-const StateIcon: React.FC<{ type: StateType; size: SizeType }> = ({
+const StateIcon: React.FC<{ type: StateType; size?: SizeType }> = ({
   type,
   size,
 }) => {
   switch (type) {
     case 'success':
-      return <CheckCircle color="#fafafa" size={size} />;
+      return <CheckCircle size={size} />;
     case 'info':
-      return <Info color="#fafafa" size={size} />;
+      return <Info size={size} />;
     case 'warning':
-      return <AlertTriangle color="#fafafa" size={size} />;
+      return <AlertTriangle size={size} />;
     case 'error':
-      return <XCircle color="#fafafa" size={size} />;
+      return <XCircle size={size} />;
     default:
       return null;
   }
@@ -28,11 +28,9 @@ const StateIcon: React.FC<{ type: StateType; size: SizeType }> = ({
 
 const State: React.FC<StateProps> = ({ type, label, size }) => {
   return (
-    <div className="p-2 flex flex-col">
-      <div className="flex justify-center">
-        <StateIcon type={type} size={size} />
-      </div>
-      <h2 className="text-lg m-2">{label}</h2>
+    <div className="flex content-center justify-center flex-col">
+      <StateIcon type={type} size={size} />
+      <h2 className="text-lg mt-2 ">{label}</h2>
     </div>
   );
 };
