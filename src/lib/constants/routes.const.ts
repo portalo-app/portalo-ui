eee44229df9786d03eeebce56e205236fe920bb0
braifz <braifz.dev@gmail.com>
2024-01-04 10:31:17 -0300

    refactor: the extra folder app was removed

--- src/lib/constants/routes.const.ts
+++ src/lib/constants/routes.const.ts
@@ -1,11 +1,10 @@
 export const ROUTES = {
-  HOME: '/',
-  APP: '/app',
-  APP_HELP: '/app/help',
-  APP_PROFILE: '/app/profile',
-  APP_CREATE_PROFILE: '/app/create-profile',
-  APP_CREATE_ADDRESS: '/app/create-address',
-  APP_SELECT_ENTITY: '/app/select-entity',
+  APP: '/',
+  APP_HELP: '/help',
+  APP_PROFILE: '/profile',
+  APP_CREATE_PROFILE: '/create-profile',
+  APP_CREATE_ADDRESS: '/create-address',
+  APP_SELECT_ENTITY: '/select-entity',
 } as const;
 
 export type ObjectValues<T> = T[keyof T];
