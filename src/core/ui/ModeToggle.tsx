6255917738da71fd88639b5a0b949ddfbecfa061
Tute <77329530+MateoEmilio1@users.noreply.github.com>
2024-06-11 16:11:20 -0300

    fix: Update ModeToggle.tsx

--- src/core/ui/ModeToggle.tsx
+++ src/core/ui/ModeToggle.tsx
@@ -1,33 +1,30 @@
 'use client';
-
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 import { Switch } from './Switch';
 
 export function ModeToggle() {
-  const [darkMode, setDarkMode] = useState<boolean>(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem('darkMode') === 'true';
-    }
-    return false;
-  });
-
+  const [darkMode, setDarkMode] = useState<boolean>(true)
   const { setTheme } = useTheme();
 
   const handleOnChecked = () => {
-    setDarkMode(!darkMode);
-  };
+    setDarkMode(!darkMode)
+  }
 
   useEffect(() => {
-    darkMode ? setTheme('dark') : setTheme('light');
-    localStorage.setItem('darkMode', darkMode.toString());
-  }, [darkMode, setTheme]);
+    darkMode ? setTheme('dark') : setTheme('light')
+  }, [darkMode, setTheme])
 
   return (
-    <div className="flex space-x-2">
+
+    <div className='flex space-x-2'>
       <Switch checked={darkMode} onCheckedChange={handleOnChecked} />
-      {darkMode ? <Moon /> : <Sun />}
+      {darkMode ? (
+        <Moon />
+      ) : (
+        <Sun />
+      )}
     </div>
   );
 }
