d1d4d3fd2d8881b07ddd571f48c03506f97bc81f
braifz <braifz.dev@gmail.com>
2024-06-11 10:54:34 -0300

    feat: pwa support

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import('@serwist/next')).default({
+      swSrc: 'public/service-worker/app-worker.ts',
+      swDest: 'public/sw.js',
+      reloadOnOnline: true,
     });
+    return withSerwist(nextConfig);
+  }
 
-    return config;
-  },
+  return nextConfig;
 };
-
-module.exports = nextConfig;
