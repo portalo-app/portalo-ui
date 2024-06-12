f13ff28790efd85f61a01ec09d2126f846b447f3
braifz <braifz.dev@gmail.com>
2024-06-12 11:26:49 -0300

    feat: show sidebar just in desktop

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,28 +1,18 @@
 'use client';
 
-import { EXTERNAL_LINKS } from '@constants/externalLinks.const';
 import { ROUTES } from '@constants/routes.const';
 import DeleteModal from '@core/components/DeleteModal';
-import { Button } from '@core/ui/Button';
-import { ModeToggle } from '@core/ui/ModeToggle';
-import { Separator } from '@core/ui/Separator';
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@core/ui/Sheet';
+  MEDIAQUERY_DESKTOP,
+  useMediaQuery,
+} from '@hooks/general/useMediaQuery';
 import { spacesState } from '@states/spaces.atom';
-import { Menu, Trash2 } from 'lucide-react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useResetRecoilState } from 'recoil';
 import AppLogo from './AppLogo';
-import DrawerMenuItems from './DrawerMenuItems';
+import Sidebar from './Sidebar';
 
 interface NavbarProps {}
 
@@ -31,11 +21,11 @@ const Navbar: React.FC<NavbarProps> = () => {
   const [resetAccountModalOpen, setResetAccountModalOpen] = useState(false);
   const router = useRouter();
 
+  const isDesktop = useMediaQuery(MEDIAQUERY_DESKTOP);
+
   const resetAccountLabel = 'Reset Account';
   const resetAccountMessage =
     'Are you sure you want to clear the account data? This will remove every Space and all the data associated with them. This action cannot be undone.';
-
-  const createdByNeoPower = 'Created by NeoPower';
 
   const resetAccount = () => {
     router.push(ROUTES.APP);
@@ -50,66 +40,17 @@ const Navbar: React.FC<NavbarProps> = () => {
 
   return (
     <>
-      <div className="sticky top-0 z-50 flex w-full items-center justify-between bg-muted p-2 pl-4">
+      <div className="sticky top-0 z-50 flex w-full items-center justify-between bg-muted p-1 pl-2">
         <Link href={ROUTES.APP}>
           <AppLogo />
         </Link>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <Menu size={24} />
-            </Button>
-          </SheetTrigger>
-
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>
-                <AppLogo />
-              </SheetTitle>
-            </SheetHeader>
-
-            <Separator />
-
-            <SheetDescription className="flex flex-col min-h-[550px] justify-between">
-              <div>
-                <DrawerMenuItems />
-
-                <div className="ml-4">
-                  <ModeToggle />
-                </div>
-              </div>
-
-              <div className="flex flex-col justify-center">
-                <Button variant="destructive" onClick={handleResetAccountModal}>
-                  <Trash2 className="mr-2" />
-
-                  {resetAccountLabel}
-                </Button>
-
-                <div className="mt-4 p-1">
-                  <Link
-                    href={EXTERNAL_LINKS.NEOPOWER}
-                    color="inherit"
-                    target="_blank"
-                  >
-                    <div className="flex justify-center flex-row items-center">
-                      <Image
-                        src="/neopower.svg"
-                        alt="neopower"
-                        width="24"
-                        height="24"
-                        style={{ marginRight: '8px' }}
-                      />
-
-                      {createdByNeoPower}
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </SheetDescription>
-          </SheetContent>
-        </Sheet>
+        {isDesktop ? (
+          <Sidebar
+            handleResetAccountModal={handleResetAccountModal}
+            resetAccountLabel={resetAccountLabel}
+          />
+        ) : null}
 
         <DeleteModal
           title={resetAccountLabel}
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,89 @@
+import { EXTERNAL_LINKS } from '@constants/externalLinks.const';
+import { Button } from '@core/ui/Button';
+import { ModeToggle } from '@core/ui/ModeToggle';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@core/ui/Sheet';
+import { Separator } from '@radix-ui/react-separator';
+import { Menu, Trash2 } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import AppLogo from './AppLogo';
+import DrawerMenuItems from './DrawerMenuItems';
+
+interface SidebarProps {
+  handleResetAccountModal: () => void;
+  resetAccountLabel: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+  handleResetAccountModal,
+  resetAccountLabel,
+}) => {
+  const createdByNeoPower = 'Created by NeoPower';
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Menu size={24} />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>
+            <AppLogo />
+          </SheetTitle>
+        </SheetHeader>
+
+        <Separator />
+
+        <SheetDescription className="flex flex-col min-h-[550px] justify-between">
+          <div>
+            <DrawerMenuItems />
+
+            <div className="ml-4">
+              <ModeToggle />
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <Button variant="destructive" onClick={handleResetAccountModal}>
+              <Trash2 className="mr-2" />
+
+              {resetAccountLabel}
+            </Button>
+
+            <div className="mt-4 p-1">
+              <Link
+                href={EXTERNAL_LINKS.NEOPOWER}
+                color="inherit"
+                target="_blank"
+              >
+                <div className="flex justify-center flex-row items-center">
+                  <Image
+                    src="/neopower.svg"
+                    alt="neopower"
+                    width="24"
+                    height="24"
+                    style={{ marginRight: '8px' }}
+                  />
+
+                  {createdByNeoPower}
+                </div>
+              </Link>
+            </div>
+          </div>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Sidebar;
