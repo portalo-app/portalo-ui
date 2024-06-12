bd0cbd53dde74c7bec2ea4f799327ebc5402596a
braifz <braifz.dev@gmail.com>
2024-06-12 11:05:48 -0300

    feat: show bottom navbar just in mobile view

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,20 +1,27 @@
+'use client';
+
 import MobileBottomNavbar from '@components/layout/MobileBottomNavbar';
 import Navbar from '@components/layout/Navbar';
 import Root from '@components/layout/Root';
+import {
+  MEDIAQUERY_DESKTOP,
+  useMediaQuery,
+} from '@hooks/general/useMediaQuery';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function AppLayout({ children }: LayoutProps) {
+  const isDesktop = useMediaQuery(MEDIAQUERY_DESKTOP);
+
   return (
     <Root>
       <Navbar />
 
       <main className="container max-w-md px-4 mt-4 pb-[70px]">{children}</main>
 
-      <MobileBottomNavbar />
-      {/* {!isDesktop ? <MobileBottomNavbar /> : null} */}
+      {!isDesktop ? <MobileBottomNavbar /> : null}
     </Root>
   );
 }
