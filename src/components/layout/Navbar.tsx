b174f5f5e8959d5e184ed2f128c21781fba4683f
braifz <braifz.dev@gmail.com>
2024-06-13 19:58:57 -0300

    fix: show the current route in header

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ROUTES_LAYOUT } from '@constants/routes.const';
+import { extractRouteFromPathname } from '@utils/utils';
 import { usePathname } from 'next/navigation';
 import FeatureHeader from './FeatureHeader';
 
@@ -8,7 +9,10 @@ interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   const pathname = usePathname();
-  const currentRoute = ROUTES_LAYOUT.find((route) => route.url === pathname);
+  const route = extractRouteFromPathname(pathname);
+  const currentRoute = ROUTES_LAYOUT.find(
+    (routeItem) => routeItem.url === route
+  );
 
   return (
     <div className="sticky top-0 z-50 flex w-full items-center justify-between bg-muted p-1 pl-2 border-b border-muted-foreground">
--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const extractRouteFromPathname = (pathname: string): string => {
+  if (pathname.includes('/vaults')) {
+    return '/vaults';
+  } else if (pathname.includes('/spaces')) {
+    return '/spaces';
+  }
+  return pathname;
+};
