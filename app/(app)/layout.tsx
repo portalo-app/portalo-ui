386ba8aa7b13ec14ade7ddcf5693d03d2aa960c0
braifz <braifz.dev@gmail.com>
2024-06-18 20:51:45 -0300

    feat: created alpha alert

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,10 +4,12 @@ import MobileBottomNavbar from '@components/layout/MobileBottomNavbar';
 import Navbar from '@components/layout/Navbar';
 import Root from '@components/layout/Root';
 import Sidebar from '@components/layout/Sidebar';
+import { Alert, AlertDescription } from '@core/ui/Alert';
 import {
   MEDIAQUERY_DESKTOP,
   useMediaQuery,
 } from '@hooks/general/useMediaQuery';
+import { Info } from 'lucide-react';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -23,6 +25,12 @@ export default function AppLayout({ children }: LayoutProps) {
         <main className="flex flex-col w-full h-screen overflow-auto">
           <Navbar />
           <div className="mt-6 self-center md:w-96 w-full px-6 md:px-0 pb-20">
+            <Alert variant={'default'} className="mb-4">
+              <AlertDescription className="flex items-center gap-2 justify-center">
+                <Info />
+                This is an Alpha version of the App
+              </AlertDescription>
+            </Alert>
             {children}
           </div>
         </main>
