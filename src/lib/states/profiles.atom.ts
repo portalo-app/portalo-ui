d0d57f612e5f8b811e55425251b0c2dc09348ae3
pcorrado-np <pcorrado@neopower.digital>
2024-07-08 00:20:57 -0300

    feat: neopower data

--- src/lib/states/profiles.atom.ts
+++ src/lib/states/profiles.atom.ts
@@ -4,6 +4,67 @@ import { localStorageEffect } from './localStorageEffect.util';
 
 export const profilesState = atom<ProfileDTO[]>({
   key: 'profilesState',
-  default: [],
+  default: [
+    {
+      id: 'neopower',
+      name: 'NeoPower',
+      folders: [
+        {
+          id: 'social',
+          folderTypeId: 'social',
+          files: [
+            {
+              data: {
+                variant: 'messaging',
+                entity: 'telegram',
+                username: '@briansasbon',
+              },
+              id: 'social_messaging_telegram',
+            },
+            {
+              data: {
+                variant: 'messaging',
+                entity: 'gmail',
+                username: 'contact@neopower.digital',
+              },
+              id: 'social_messaging_gmail',
+            },
+            {
+              data: {
+                variant: 'social-media',
+                entity: 'x',
+                username: 'NeoPowerDigital',
+              },
+              id: 'social_social-media_x',
+            },
+          ],
+        },
+        {
+          id: 'address',
+          folderTypeId: 'address',
+          files: [
+            {
+              data: {
+                variant: 'crypto',
+                entity: 'MATIC',
+                address: '0x39bcd81c6db7f3672a1a0e8e08e3c3a5d93d03ff',
+                name: 'NeoPower Polygon wallet',
+              },
+              id: 'address_crypto_MATIC',
+            },
+            {
+              data: {
+                variant: 'crypto',
+                entity: 'ATOM',
+                address: 'cosmos1ct4d6m6av7w25ygcrtnx00r2ezrklh9rgc3wmk',
+                name: 'NeoPower Cosmos wallet',
+              },
+              id: 'address_crypto_ATOM',
+            },
+          ],
+        },
+      ],
+    },
+  ],
   effects: [localStorageEffect('portalo.profiles')],
 });
