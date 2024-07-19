7cf90d0a032fa5a9edbe0ab122aa17b61d196ecd
juanarancibia <arancibia.juan22@gmail.com>
2024-07-19 11:47:48 -0300

    feat: connect wallet with metamask

--- app/(app)/lighthouse/page.tsx
+++ app/(app)/lighthouse/page.tsx
@@ -2,9 +2,25 @@
 
 import { Button } from '@core/ui/Button';
 import lighthouse from '@lighthouse-web3/sdk';
+import { ethers } from 'ethers';
 import { NextPage } from 'next';
+import { useState } from 'react';
 
 const Page: NextPage = () => {
+  const [account, setAccount] = useState(null);
+
+  const connectToMetaMask = async () => {
+    if ((window as any).ethereum) {
+      const provider = new ethers.providers.Web3Provider(
+        (window as any).ethereum
+      );
+      const accounts = await provider.send('eth_requestAccounts', []);
+      setAccount(accounts[0]);
+    } else {
+      alert('MetaMask is not installed');
+    }
+  };
+
   const saveFile = async () => {
     const apiKey = process.env.NEXT_PUBLIC_LIGHTHOUSE_API_KEY!;
     const uploadResponse = await lighthouse.uploadText(
@@ -33,7 +49,10 @@ const Page: NextPage = () => {
   return (
     <div className="flex flex-col gap-3 items-center">
       <h1>Lighthouse</h1>
-      <p>Page with lighthouse score of 100</p>
+
+      <Button onClick={connectToMetaMask}>Connect to MetaMask</Button>
+      {account && <p>Connected account: {account}</p>}
+
       <Button onClick={() => saveFile()}>Press to Save File</Button>
       <Button onClick={() => getUploadedFiles()}>Get uploaded files</Button>
       <Button onClick={() => getFileByCID()}>Get File</Button>
