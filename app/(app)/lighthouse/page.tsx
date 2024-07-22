e1cd4e5122eece02647ffa9de02aebe9397792a4
juanarancibia <arancibia.juan22@gmail.com>
2024-07-22 15:07:31 -0300

    feat: basic functionality working

--- app/(app)/lighthouse/page.tsx
+++ app/(app)/lighthouse/page.tsx
@@ -4,10 +4,11 @@ import { Button } from '@core/ui/Button';
 import lighthouse from '@lighthouse-web3/sdk';
 import { ethers } from 'ethers';
 import { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Page: NextPage = () => {
-  const [account, setAccount] = useState(null);
+  const [account, setAccount] = useState<string>('');
+  const [authMessage, setAuthMessage] = useState('');
 
   const connectToMetaMask = async () => {
     if ((window as any).ethereum) {
@@ -21,12 +22,38 @@ const Page: NextPage = () => {
     }
   };
 
-  const saveFile = async () => {
+  const lighthouseAuth = async () => {
+    const messageToSign = await (
+      await fetch(
+        `https://encryption.lighthouse.storage/api/message/${account}`
+      )
+    ).json();
+
+    setAuthMessage(messageToSign[0].message);
+    console.log(messageToSign[0].message);
+  };
+
+  const signMessage = async (): Promise<string> => {
+    const provider = new ethers.providers.Web3Provider(
+      (window as any).ethereum
+    );
+    const signer = provider.getSigner();
+    return await signer.signMessage(authMessage);
+  };
+
+  const saveEncryptedFile = async () => {
     const apiKey = process.env.NEXT_PUBLIC_LIGHTHOUSE_API_KEY!;
-    const uploadResponse = await lighthouse.uploadText(
-      'This is a Lighthouse test',
+
+    const signedMessage = await signMessage();
+
+    console.log(signedMessage);
+
+    const uploadResponse = await lighthouse.textUploadEncrypted(
+      'This is a Lighthouse test encrypted',
       apiKey,
-      'testLighthouse'
+      account!,
+      signedMessage,
+      'testLighthouseEncrypted'
     );
 
     console.log(uploadResponse);
@@ -40,22 +67,106 @@ const Page: NextPage = () => {
   };
 
   const getFileByCID = async () => {
-    const cid = 'bafkreigz3m7a4om2l2vtzinjavyl7b7s5a3uh4fms57tjrjjt5vvduokvq';
+    const cid = 'bafkreicxgzu5xzxnh66bue557ondbn6lskpsfc4xtho6zsbqi6w7pp53iq';
     const file = await fetch(`https://gateway.lighthouse.storage/ipfs/${cid}`);
 
     console.log(await file.text());
+  };
+
+  const readEncryptedFile = async () => {
+    const cid = 'bafkreicxgzu5xzxnh66bue557ondbn6lskpsfc4xtho6zsbqi6w7pp53iq';
+    const signedMessage = await signMessage();
+
+    const fileEncryptionKey = await lighthouse.fetchEncryptionKey(
+      cid,
+      account!,
+      signedMessage
+    );
+
+    const decrypted = await lighthouse.decryptFile(
+      cid,
+      fileEncryptionKey!.data!.key!
+    );
+
+    console.log(await decrypted.text());
+  };
+
+  const shareEncryptedFile = async () => {
+    const cid = 'bafkreicxgzu5xzxnh66bue557ondbn6lskpsfc4xtho6zsbqi6w7pp53iq';
+    const signedMessage = await signMessage();
+    const receiverPk = '0x4795B7bfB19D804b68E921ADbf2a5134467eEBf6';
+
+    const shareResponse = await lighthouse.shareFile(
+      account!,
+      [receiverPk],
+      cid,
+      signedMessage
+    );
+
+    console.log({ shareResponse });
   };
 
   return (
     <div className="flex flex-col gap-3 items-center">
       <h1>Lighthouse</h1>
 
-      <Button onClick={connectToMetaMask}>Connect to MetaMask</Button>
+      {!account && (
+        <Button onClick={connectToMetaMask}>Connect to MetaMask</Button>
+      )}
       {account && <p>Connected account: {account}</p>}
+      {account && (
+        <AccountSelector
+          account={account}
+          setAccount={(account: string) => setAccount(account)}
+        />
+      )}
 
-      <Button onClick={() => saveFile()}>Press to Save File</Button>
+      {account && (
+        <Button onClick={() => lighthouseAuth()}>Lighthouse Auth</Button>
+      )}
+      <Button onClick={() => saveEncryptedFile()}>Press to Save File</Button>
       <Button onClick={() => getUploadedFiles()}>Get uploaded files</Button>
       <Button onClick={() => getFileByCID()}>Get File</Button>
+      <Button onClick={() => readEncryptedFile()}>Read Encrypted File</Button>
+      <Button onClick={() => shareEncryptedFile()}>Share Encrypted File</Button>
+    </div>
+  );
+};
+
+const AccountSelector: React.FC<{
+  account: string;
+  setAccount: (account: string) => void;
+}> = ({ account, setAccount }) => {
+  const [accounts, setAccounts] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      if ((window as any).ethereum) {
+        const provider = new ethers.providers.Web3Provider(
+          (window as any).ethereum
+        );
+        const accounts = await provider.send('eth_requestAccounts', []);
+        setAccounts(accounts);
+      }
+    };
+
+    fetchAccounts();
+  }, []);
+
+  const handleAccountChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setAccount(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="account">Select Account:</label>
+      <select id="account" value={account} onChange={handleAccountChange}>
+        {accounts.map((acc) => (
+          <option key={acc} value={acc}>
+            {acc}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
