f16b8f45bd340dc8f412d9ac602adbaeef4c98eb
braifz <braifz.dev@gmail.com>
2024-07-30 16:18:20 -0300

    feat: add pwa support

--- next.config.js
+++ next.config.js
@@ -1,19 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withMDX = require('@next/mdx')();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import('@serwist/next')).default({
+      // Note: This is only an example. If you use Pages Router,
+      // use something else that works, such as "service-worker/index.ts".
+      swSrc: 'app/sw.ts',
+      swDest: 'public/sw.js',
     });
+    return withSerwist(withMDX(nextConfig));
+  }
 
-    return config;
-  },
+  return withMDX(nextConfig);
 };
-
-module.exports = withMDX(nextConfig);
