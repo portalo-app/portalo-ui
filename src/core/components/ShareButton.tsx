76dbc6f1f1fb9e7dade96a79a6c1ffe6026f4457
Brian Sasbon <bsasbon@neopower.digital>
2024-08-03 02:09:06 -0300

    feat: add tabs for local and cloud

--- src/core/components/ShareButton.tsx
+++ src/core/components/ShareButton.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@core/ui/Button';
+import { Card } from '@core/ui/Card';
 import ResponsiveDialog from '@core/ui/ResponsiveDialog';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@core/ui/Tab';
 import {
   TypographyH4,
   TypographyMutedXS,
@@ -7,9 +9,17 @@ import {
 } from '@core/ui/Typography';
 import { ProfileDTO } from '@models/dto/profile.dto';
 import Avvvatars from 'avvvatars-react';
-import { Share, Share2, Users } from 'lucide-react';
+import {
+  Cloud,
+  HardDrive,
+  Share,
+  Share2,
+  TriangleAlert,
+  Users,
+} from 'lucide-react';
+import lzString from 'lz-string';
 import { QRCodeSVG } from 'qrcode.react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import CopyButton from './CopyButton';
 
 interface ShareButtonProps {
@@ -17,18 +27,22 @@ interface ShareButtonProps {
 }
 
 const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
-  // const profileToShare = {
-  //   ...profile,
-  //   folders: profile.folders.filter((folder) => folder.files.length > 0),
-  // };
-  // const JSONProfile = JSON.stringify(profileToShare);
-  // const compressed = lzString.compressToEncodedURIComponent(JSONProfile);
+  const [selectedTab, setSelectedTab] = useState('cloud');
 
-  // const shareUrl = `${window.location.origin}/profiles/share?profile=${compressed}`;
+  const profileToShare = {
+    ...profile,
+    folders: profile.folders.filter((folder) => folder.files.length > 0),
+  };
+  const JSONProfile = JSON.stringify(profileToShare);
+  const compressed = lzString.compressToEncodedURIComponent(JSONProfile);
+
+  const localURL = `${window.location.origin}/profiles/share?profile=${compressed}`;
+
+  // TODO: Model 'OFFLINE' or 'CLOUD'
+  const profileStatus = 'OFFLINE';
 
   // TODO: Set the correct and UNIQUE profile ID
-  const profileID = profile.id;
-  const shareUrl = `${window.location.origin}/profiles/share?profile=${profileID}`;
+  const cloudURL = `${window.location.origin}/profiles/share?id=${profile.id}`;
 
   // TODO: Implement
   const goToManageAccess = () => {
@@ -43,9 +57,18 @@ const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
     // });
   };
 
+  // TODO: Implement
+  const goToUpload = () => {
+    // router.push(`/profiles/${profile.id}/upload`);
+  };
+
+  const handleTabChange = (value: string) => {
+    setSelectedTab(value);
+  };
+
   return (
     <ResponsiveDialog
-      title="Share Profile"
+      title=""
       trigger={
         <Button
           variant="outline"
@@ -62,42 +85,68 @@ const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
           <div className="flex items-center gap-2">
             <Avvvatars value={profile.name} size={42} />
 
-            <TypographyH4 className="text-left">
-              {profile.name}&apos;s profile
-            </TypographyH4>
+            <TypographyH4 className="text-left">{profile.name}</TypographyH4>
           </div>
         </div>
 
-        <div className="px-4 pt-2 space-y-2 w-full">
-          <QRCodeSVG
-            includeMargin
-            value={shareUrl}
-            size={512}
-            className="w-full h-fit rounded-3xl"
-          />
+        <Tabs
+          className="px-4 pt-2"
+          defaultValue={selectedTab}
+          onValueChange={handleTabChange}
+        >
+          <TabsList className="w-full border border-muted rounded-full h-10 px-1 bg-muted/25 *:flex-1 *:gap-2">
+            <TabsTrigger value="cloud">
+              <Cloud />
+              Cloud
+            </TabsTrigger>
 
-          <TypographyMutedXS>{shareUrl}</TypographyMutedXS>
-        </div>
+            <TabsTrigger value="local">
+              <HardDrive />
+              Local
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="cloud">
+            <div className="w-full space-y-2">
+              {profileStatus === 'OFFLINE' && (
+                <Card
+                  className="flex items-center p-2 gap-2 text-destructive border-destructive"
+                  onClick={goToUpload}
+                >
+                  <TriangleAlert />
+
+                  <Button variant="link" className="text-destructive">
+                    Click to upload this profile to the cloud
+                  </Button>
+                </Card>
+              )}
+
+              <QRCodeSVG
+                includeMargin
+                value={cloudURL}
+                size={512}
+                className="w-full h-fit rounded-3xl"
+              />
+
+              <TypographyMutedXS>{cloudURL}</TypographyMutedXS>
+            </div>
+          </TabsContent>
+
+          <TabsContent value="local">
+            <div className="w-full space-y-2">
+              <QRCodeSVG
+                includeMargin
+                value={localURL}
+                size={512}
+                className="w-full h-fit rounded-3xl"
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
 
         <div className="flex flex-col gap-3 w-full pb-3 pt-5">
-          {navigator?.share && (
-            <Button
-              className="gap-2 w-full"
-              variant="outline"
-              onClick={() =>
-                navigator?.share({
-                  text: `👛️ Portalo: ${profile.name}'s profile`,
-                  url: shareUrl,
-                })
-              }
-            >
-              <Share2 size={16} />
-              Share
-            </Button>
-          )}
-
           <div className="flex *:flex-1 gap-2">
-            <CopyButton text={shareUrl} />
+            <CopyButton text={cloudURL} />
 
             <Button variant="outline" className="gap-2" onClick={shareProfile}>
               <Share2 />
@@ -105,10 +154,12 @@ const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
             </Button>
           </div>
 
-          <Button className="gap-2" onClick={goToManageAccess}>
-            <Users />
-            Manage Access
-          </Button>
+          {selectedTab === 'cloud' && (
+            <Button className="gap-2" onClick={goToManageAccess}>
+              <Users />
+              Manage Access
+            </Button>
+          )}
         </div>
       </div>
     </ResponsiveDialog>
