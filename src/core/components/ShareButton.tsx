e7c3ca5978e69527ac633742b3fac04561986628
Brian Sasbon <bsasbon@neopower.digital>
2024-08-03 00:39:37 -0300

    feat: add buttons and comment out

--- src/core/components/ShareButton.tsx
+++ src/core/components/ShareButton.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@core/ui/Button';
 import ResponsiveDialog from '@core/ui/ResponsiveDialog';
-import { TypographyH4, TypographyP } from '@core/ui/Typography';
+import {
+  TypographyH4,
+  TypographyMutedXS,
+  TypographyP,
+} from '@core/ui/Typography';
 import { ProfileDTO } from '@models/dto/profile.dto';
 import Avvvatars from 'avvvatars-react';
-import { Share, Share2 } from 'lucide-react';
-import lzString from 'lz-string';
+import { Share, Share2, Users } from 'lucide-react';
 import { QRCodeSVG } from 'qrcode.react';
 import { FC } from 'react';
 import CopyButton from './CopyButton';
@@ -14,14 +17,31 @@ interface ShareButtonProps {
 }
 
 const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
-  const profileToShare = {
-    ...profile,
-    folders: profile.folders.filter((folder) => folder.files.length > 0),
-  };
-  const JSONProfile = JSON.stringify(profileToShare);
-  const compressed = lzString.compressToEncodedURIComponent(JSONProfile);
+  // const profileToShare = {
+  //   ...profile,
+  //   folders: profile.folders.filter((folder) => folder.files.length > 0),
+  // };
+  // const JSONProfile = JSON.stringify(profileToShare);
+  // const compressed = lzString.compressToEncodedURIComponent(JSONProfile);
 
-  const shareUrl = `${window.location.origin}/profiles/share?profile=${compressed}`;
+  // const shareUrl = `${window.location.origin}/profiles/share?profile=${compressed}`;
+
+  // TODO: Set the correct and UNIQUE profile ID
+  const profileID = profile.id;
+  const shareUrl = `${window.location.origin}/profiles/share?profile=${profileID}`;
+
+  // TODO: Implement
+  const goToManageAccess = () => {
+    // router.push(`/profiles/${profile.id}/manage-access`);
+  };
+
+  // TODO: Implement
+  const shareProfile = () => {
+    // navigator?.share({
+    //   text: `Portalo profile: ${profile.name}`,
+    //   url: shareUrl,
+    // });
+  };
 
   return (
     <ResponsiveDialog
@@ -55,6 +75,8 @@ const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
             size={512}
             className="w-full h-fit rounded-3xl"
           />
+
+          <TypographyMutedXS>{shareUrl}</TypographyMutedXS>
         </div>
 
         <div className="flex flex-col gap-3 w-full pb-3 pt-5">
@@ -73,7 +95,20 @@ const ShareButton: FC<ShareButtonProps> = ({ profile }) => {
               Share
             </Button>
           )}
-          <CopyButton text={shareUrl} />
+
+          <div className="flex *:flex-1 gap-2">
+            <CopyButton text={shareUrl} />
+
+            <Button variant="outline" className="gap-2" onClick={shareProfile}>
+              <Share2 />
+              Share
+            </Button>
+          </div>
+
+          <Button className="gap-2" onClick={goToManageAccess}>
+            <Users />
+            Manage Access
+          </Button>
         </div>
       </div>
     </ResponsiveDialog>
