bb2709fbd45754b131ac164c8ad8c86112a47cac
pcorrado-np <pcorrado@neopower.digital>
2024-08-04 11:29:00 -0300

    build: configure scroll sepolia chain

--- src/lib/utils/wagmiConfig.ts
+++ src/lib/utils/wagmiConfig.ts
@@ -2,7 +2,7 @@ import { createConfig, http } from 'wagmi';
 import { mainnet, scrollSepolia, sepolia } from 'wagmi/chains';
 
 export const wagmiConfig = createConfig({
-  chains: [sepolia, scrollSepolia],
+  chains: [scrollSepolia],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
